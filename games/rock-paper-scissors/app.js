8a1d4f776fb8254807c51e646261daa172e8b5cb
armaneousuf <armaneousuf97@gmail.com>
2026-04-12 23:40:25 +0600

    feat: improved and shortned conditionals

--- games/rock-paper-scissors/app.js
+++ games/rock-paper-scissors/app.js
@@ -1,41 +1,39 @@
-const computerChoice = document.querySelector('#computer-choice');
-const userChoice = document.querySelector('#user-choice');
-const result = document.querySelector('#result');
+const computerChoice = document.querySelector("#computer-choice");
+const userChoice = document.querySelector("#user-choice");
+const result = document.querySelector("#result");
 
 // console.log(computerChoice, userChoice, result)
 
-
-const btns = document.querySelectorAll('.btns button');
+const btns = document.querySelectorAll(".btns button");
 btns.forEach((btn) => {
-    btn.addEventListener('click', (e)=> {
-        // console.log(btn, e)
-        userChoice.textContent = e.target.textContent;
-      generateComputerChoice()
-      getResult()
-    })
-})
+  btn.addEventListener("click", (e) => {
+    // console.log(btn, e)
+    userChoice.textContent = e.target.textContent;
+    generateComputerChoice();
+    getResult();
+  });
+});
 
-function generateComputerChoice () {
-    const randomNumber = Math.floor(Math.random() * btns.length);
-    // console.log(randomNumber)
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * btns.length);
+  // console.log(randomNumber)
 
-    computerChoice.textContent = btns[randomNumber].textContent
-
+  computerChoice.textContent = btns[randomNumber].textContent;
 }
 
-function getResult () {
- if (computerChoice.textContent === userChoice.textContent){
-    result.textContent = "Tied";
- } 
-  else if (computerChoice.textContent === 'Rock' && userChoice.textContent === 'Scissors') {
-    result.textContent = 'You lose'  
- } 
- else if (computerChoice.textContent === 'Scissors' && userChoice.textContent === 'Paper') {
-    result.textContent = 'You lose';
- } 
- else if (computerChoice.textContent === 'Paper' && userChoice.textContent === 'Rock'){
-    result.textContent = 'You lose'
- } else {
-    result.textContent = 'You Win'
- }
+function getResult() {
+  const computer = computerChoice.textContent;
+  const user = userChoice.textContent;
+
+  if (computerChoice.textContent === userChoice.textContent) {
+    result.textContent = "It's a Tie! 🤝";
+  } else if (
+    (computer === "Rock" && user === "Scissors") ||
+    (computer === "Scissors" && user === "Paper") ||
+    (computer === "Paper" && user === "Rock")
+  ) {
+    result.textContent = "You lose! 💀";
+  } else {
+    result.textContent = "You Win! 🏆";
+  }
 }
