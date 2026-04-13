6865528c09a19d82e24afc0bd0e10041492282ac
armaneousuf <armaneousuf97@gmail.com>
2026-04-13 10:33:08 +0600

    update: 2026-04-13 10:33

--- games/memory-game/app.js
+++ games/memory-game/app.js
@@ -1,1 +1,43 @@
-console.log('hello world')
+const cardArray = [
+    {
+        name: 'fries', 
+        img: 'images/fries.png'
+    }, 
+    {
+        name: 'cheeseburger', 
+        img: 'images/cheeseburger.png'
+    }, 
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    }, 
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }, 
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    }
+];
+cardArray.sort(()=> 0.5 - Math.random());
+// console.log(cardArray)
+
+const gridDisplay = document.querySelector('.grid')
+// console.log(gridDisplay)
+
+function cardBoard () {
+    for (let i = 0; i < 10; i++){
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.png');
+        card.setAttribute('data-id', i)
+        console.log(card)
+        gridDisplay.append(card)
+    }
+};
+
+cardBoard()
