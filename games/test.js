a871c2d76ec230b3fdc62fbf5b0c841d3db0b577
armaneousuf <armaneousuf97@gmail.com>
2026-04-13 22:47:09 +0600

    update: 2026-04-13 22:47

---
+++ games/test.js
@@ -0,0 +1,11 @@
+const output = "what's your perfect first date?".split("")
+.map(parseInt)
+.filter(a => a)
+.reduce((a, b) => a+b)
+.toString()
+.split("")
+.reverse()
+.join("")
+
+
+console.log(output);
