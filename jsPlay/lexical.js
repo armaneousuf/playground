183709109d9303e917ead0e2c88813183fb356a1
armaneousuf <armaneousuf97@gmail.com>
2026-04-19 00:06:17 +0600

    update: 2026-04-19 00:06

--- jsPlay/lexical.js
+++ jsPlay/lexical.js
@@ -1,12 +1,28 @@
-var x = "Global";
+// var x = "Global";
 
-function a(){
-    console.log(x);
-};
+// function a(){
+//     console.log(x);
+// };
 
-function b(){
-    var x = "inside b";
-    a()
+// function b(){
+//     var x = "inside b";
+//     a()
+// }
+
+// b();
+
+
+let a = "Global";
+function level1(){
+    let b = "level 1";
+
+    function level2() {
+        let c = "level 2";
+        console.log(a)
+        console.log(b)
+        console.log(c)
+        console.log(d)
+    }
+    level2()
 }
-
-b();
+level1()
