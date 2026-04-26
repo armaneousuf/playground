9a41b61d7059b0e92a08f46425c5172874e749f7
armaneousuf <armaneousuf97@gmail.com>
2026-04-26 22:31:28 +0600

    update: 2026-04-26 22:31

--- jsPlay/function/tasks/task-11.js
+++ jsPlay/function/tasks/task-11.js
@@ -14,3 +14,17 @@ const triple = multiplier(3); // another new machine and 3 is the argument for f
 
 console.log(double(5));  // since factor already has it's argument so 5 is now argument for 
 console.log(triple(5)); // same as before. double holds multiplier which is factor's arg and calling double means setting up number's arg
+
+
+function greeter(greeting) { // greeting factory that will produce greetings
+  return function(name) { // closure and it will be our second parameter 'name'
+    return `${greeting}, ${name}!`; // return the result of both of the functions
+  };
+}
+
+const sayHello = greeter('Hello'); // new machine, completely independent and also the arg for factory param 'greeting'
+const sayGood = greeter('Good morning'); // same as the before but with different values
+
+console.log(sayHello('Yousuf'));    // Hello, Yousuf! and also arg for 'name'
+console.log(sayGood('Yousuf'));     // Good morning, Yousuf! 
+console.log(sayHello('Ali'));       // Hello, Ali! same as before. value inside them doesn't matter but calling the function does
