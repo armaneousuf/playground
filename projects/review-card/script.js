6805c002cefdde890817294c715c7af1c2bb06c3
armaneousuf <armaneousuf97@gmail.com>
2026-04-27 18:48:38 +0600

    update: 2026-04-27 18:48

--- projects/review-card/script.js
+++ projects/review-card/script.js
@@ -2,31 +2,31 @@ const persons = [
     {
         name: 'Alison', 
         title: 'Software Engineer', 
-        url: 'https://images.unsplash.com/photo-1776445602573-0cc8680b4d0a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        url: 'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?q=80&w=843&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ullam.'
     }, 
     {
         name: 'John Doe', 
         title: 'Human Resource', 
-        url: 'https://images.unsplash.com/photo-1776445602573-0cc8680b4d0a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        url: 'https://images.unsplash.com/photo-1573865526739-10659fec78a5?q=80&w=415&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ullam.'
     }, 
     {
         name: 'Emily', 
         title: 'Content Manager', 
-        url: 'https://images.unsplash.com/photo-1776445602573-0cc8680b4d0a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        url: 'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ullam.'
     }, 
     {
         name: 'Arman', 
         title: 'Software Engineer', 
-        url: 'https://images.unsplash.com/photo-1776445602573-0cc8680b4d0a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        url: 'https://images.unsplash.com/photo-1561948955-570b270e7c36?q=80&w=301&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ullam.'
     }, 
     {
         name: 'Lucy', 
         title: 'Graphics Desiner', 
-        url: 'https://images.unsplash.com/photo-1776445602573-0cc8680b4d0a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        url: 'https://images.unsplash.com/photo-1574158622682-e40e69881006?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, ullam.'
     }
 ];
