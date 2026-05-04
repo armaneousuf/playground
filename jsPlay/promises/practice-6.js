4ca7dbf8c5cdbc9da2cf8cfe483b812b86c15af4
armaneousuf <armaneousuf97@gmail.com>
2026-05-04 11:03:28 +0600

    update: 2026-05-04 11:03

---
+++ jsPlay/promises/practice-6.js
@@ -0,0 +1,15 @@
+const getUser = fetch("https://jsonplaceholder.typicode.com/users/1").then(
+  (res) => res.json(),
+);
+
+const getPost = fetch("https://jsonplaceholder.typicode.com/posts/1").then(
+  (res) => res.json(),
+);
+
+const getComments = fetch(
+  "https://jsonplaceholder.typicode.com/comments/1",
+).then((res) => res.json());
+
+Promise.allSettled([getUser, getPost, getComments])
+.then((value) => console.log(value))
+.catch((error) => console.log(error))
