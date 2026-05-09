0e69de3b1d20db36550e517a31b80788a69b8313
armaneousuf <armaneousuf97@gmail.com>
2026-05-09 22:29:48 +0600

    update: 2026-05-09 22:29

---
+++ domPlayground/lesson4/script.js
@@ -0,0 +1,5 @@
+document.querySelector("h1").textContent = "Hello world from DOM";
+document.body.style.backgroundColor = 'lightblue';
+
+document.getElementById('btn').addEventListener('click', handleclick);
+
