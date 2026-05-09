534e3937e98873c9cf42a267addfd008c07cbdae
armaneousuf <armaneousuf97@gmail.com>
2026-05-09 23:07:58 +0600

    update: 2026-05-09 23:07

--- domPlayground/lesson4/script.js
+++ domPlayground/lesson4/script.js
@@ -3,3 +3,14 @@ document.body.style.backgroundColor = 'lightblue';
 
 document.getElementById('btn').addEventListener('click', handleclick);
 
+function handleclick() {
+    alert('Button clicked!');
+}
+
+document.title = 'DOM Manipulation';
+
+const newItem = document.createElement('div');
+newItem.className = 'item';
+document.body.append(newItem);
+
+document.querySelector('.item').textContent = 'This div created by JavaScript';
