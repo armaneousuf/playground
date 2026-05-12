1960ccf75d474508586ea971dba3b54e40f27c60
armaneousuf <armaneousuf97@gmail.com>
2026-05-12 13:59:50 +0600

    update: 2026-05-12 13:59

--- vanillaJSProjects/color flipper/script.js
+++ vanillaJSProjects/color flipper/script.js
@@ -1,18 +1,19 @@
-const color = document.querySelector('.color');
-const btn = document.querySelector('.btn');
+const color = document.querySelector(".color");
+const btn = document.querySelector(".btn");
 
-const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+const hexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+function generateHexColor() {
+  let hex = "#";
+  for (let i = 0; i < 6; i++) {
+    const random = Math.floor(Math.random() * hexColor.length);
+    hex += hexColor[random];
+  }
+  return hex;
+}
 
 btn.addEventListener('click', () => {
-    let hex = '#';
-    for (let i = 0; i < 6; i++){
-        hex += hexColor[randomColorGenerator()];
-    }
-
-    document.body.style.backgroundColor = hex;
-    color.textContent = hex;
-});
-
-function randomColorGenerator(){
-    return random = Math.floor(Math.random() * hexColor.length)
-}
+    const newColor = generateHexColor();
+    color.textContent = newColor;
+    document.body.style.backgroundColor = newColor;
+})
