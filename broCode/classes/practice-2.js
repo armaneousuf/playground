49f747b1a86c9166a46154480435ef0a2637c086
armaneousuf <armaneousuf97@gmail.com>
2026-07-22 23:53:23 +0600

    update: 2026-07-22 23:53

--- broCode/classes/practice-2.js
+++ broCode/classes/practice-2.js
@@ -3,6 +3,11 @@ class Vehicle {
         this.make = make;
         this.model = model;
     }
+
+    startEngine(){
+        return `The ${this.make} ${this.model}'s engine is running!`;
+    }
+    
 }
 
 class Car extends Vehicle {
@@ -17,16 +22,22 @@ class Plane extends Vehicle {
         super(make, model);
         this.maxAltitude = maxAltitude;
     }
+
+    fly() {
+        return `The ${this.make} ${this.model} is flying at ${this.maxAltitude} feet!`
+    }
 }
 
 const car = new Car ('Toyota', 'Corolla', 4);
 const plane = new Plane('Boeing', 747, 34000);
 
-console.log(car);
-console.log(car.make);
-console.log(car.model);
-console.log(car.numDoors);
-console.log(plane);
-console.log(plane.make);
-console.log(plane.model);
-console.log(plane.maxAltitude);
+// console.log(car);
+// console.log(car.make);
+// console.log(car.model);
+// console.log(car.numDoors);
+// console.log(plane);
+// console.log(plane.make);
+// console.log(plane.model);
+// console.log(plane.maxAltitude);
+
+console.log(car.startEngine());
