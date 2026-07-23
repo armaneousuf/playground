fd442e9547e00fabd1f6b785a00b196d7f56ba67
armaneousuf <armaneousuf97@gmail.com>
2026-07-23 10:25:12 +0600

    update: 2026-07-23 10:25

---
+++ broCode/destructuring/practice-2.js
@@ -0,0 +1,15 @@
+const person = {
+  fullName: "Sarah Conor",
+  role: "Defender",
+};
+
+const { fullName: name, role: role } = person;
+
+console.log(name);
+
+
+function displayScore({name, score}) {
+  console.log(`${name} scored ${score} points!`);
+}
+
+displayScore({ name: "Alex", score: 95 });
