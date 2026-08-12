8f690ed246f624c18da1154e1444355af342bc4f
armaneousuf <armaneousuf97@gmail.com>
2026-08-12 11:41:16 +0600

    update: 2026-08-12 11:41

---
+++ broCode/DOM/weather app/script.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector('.weather-form');
+const cityInput = document.querySelector('.city-input');
+const card = document.querySelector('.card');
+const API = 'f109ba4cdde76e09b59670556ad49b9d';
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+});
+
+async function getWeatherData(city) {
+    
+}
+
+function displayWeatherInfo(data){
+
+}
+
+function getWeatherEmoji(weatherId){
+
+}
+
+function errorMessage(message){
+
+}
