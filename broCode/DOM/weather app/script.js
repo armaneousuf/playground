a95aba2b38436b4594a4ccbdb4da1c6851ea3000
armaneousuf <armaneousuf97@gmail.com>
2026-08-12 17:36:23 +0600

    update: 2026-08-12 17:36

--- broCode/DOM/weather app/script.js
+++ broCode/DOM/weather app/script.js
@@ -1,25 +1,42 @@
-const weatherForm = document.querySelector('.weather-form');
-const cityInput = document.querySelector('.city-input');
-const card = document.querySelector('.card');
-const API = 'f109ba4cdde76e09b59670556ad49b9d';
+const weatherForm = document.querySelector(".weather-form");
+const cityInput = document.querySelector(".city-input");
+const card = document.querySelector(".card");
+const API = "f109ba4cdde76e09b59670556ad49b9d";
 
-weatherForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
+weatherForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const city = cityInput.value;
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherInfo(weatherData);
+    } catch (error) {
+      console.log(error);
+      displayError(error);
+    }
+  } else {
+    displayError("Please enter a city");
+  }
 });
 
 async function getWeatherData(city) {
-    
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
+
+    const response = await fetch(apiUrl);
+    console.log(response);
 }
 
-function displayWeatherInfo(data){
+function displayWeatherInfo(data) {}
 
-}
+function getWeatherEmoji(weatherId) {}
 
-function getWeatherEmoji(weatherId){
+function displayError(message) {
+  const errorMessage = document.createElement("p");
+  errorMessage.textContent = message;
+  errorMessage.classList.add("err-display");
 
-}
-
-function errorMessage(message){
-
+  card.textContent = ""; 
+  card.style.display = "flex";
+  card.appendChild(errorMessage);
 }
