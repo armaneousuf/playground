c70fadfe031d4d1c0ced31d4bfad7d804ec3a94f
armaneousuf <armaneousuf97@gmail.com>
2026-08-23 23:58:23 +0600

    update: 2026-08-23 23:58

---
+++ jsFoundation/functions/practice-1.js
@@ -0,0 +1,8 @@
+const roundTo = function (n, step) {
+  let reminder = n % step;
+  console.log(reminder);
+  return n - reminder + (reminder < step / 2 ? 0 : step);
+};
+
+console.log(roundTo(48, 5));
+console.log(48 - 3);
