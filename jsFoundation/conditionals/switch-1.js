a0371f6ac351a97509a524f706fb728fafc3aa97
armaneousuf <armaneousuf97@gmail.com>
2026-08-23 16:50:15 +0600

    update: 2026-08-23 16:50

---
+++ jsFoundation/conditionals/switch-1.js
@@ -0,0 +1,16 @@
+let a = 3;
+
+switch (a) {
+  case 4:
+    console.log("Right");
+    break;
+
+  case 3:
+  case 5:
+    console.log("Wrong!");
+    console.log("Why don't you take a math class?");
+    break;
+
+  default:
+    console.log("The result is strange. Really.");
+}
